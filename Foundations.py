b4aee916ee6fdedd2883754f86dd744d4ebad02f
Thomas Meere <52209512+Tmeere@users.noreply.github.com>
2025-04-01 18:55:37 -0400

    Create Foundations.py

---
+++ Foundations.py
@@ -0,0 +1,18 @@
+
+#  Odd or even function
+def even_or_odd(number):
+  if number % 2 == 0:
+    return "Even"
+  else:
+    return "Odd"
+
+# Converting an number to an String
+def number_to_string(num):
+    return str(num)
+
+
+# remove the empty space in the X
+def no_space(x):
+    return x.replace(" ", "")
+  
+  
